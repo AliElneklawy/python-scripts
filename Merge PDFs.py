603bb5fa38e15509ff52585ec6d1f8d68052a4fd
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-30 10:43:38 +0200

    Add files via upload

---
+++ Merge PDFs.py
@@ -0,0 +1,17 @@
+from heapq import merge
+from os import listdir
+from PyPDF2 import PdfFileMerger
+print(""" 
+		Put all the files you want to merge in one folder
+ """)
+path = input("Enter the path to the file with the PDFs: ")
+PDFs = []
+merger = PdfFileMerger()
+for file in listdir(path):
+	if file.endswith(".pdf"):
+		PDFs.append(file )
+print("Merging files....")
+for pdf in PDFs:
+	merger.append(path + '\\' + pdf)
+merger.write(f"{path}\merged.pdf")
+merger.close()
